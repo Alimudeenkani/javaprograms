3cb34126aa8cb964be6164a8c30ef7e68966fff3
Alimudeenkani <146505233+Alimudeenkani@users.noreply.github.com>
2025-08-19 16:47:38 +0530

    Add files via upload

---
+++ javacasting.java
@@ -0,0 +1,19 @@
+package javacasting;
+
+public class javacasting {
+
+	public static void main(String[] args) {
+	
+	int myInt=9;
+	double myDouble=myInt;
+	System.out.println("the value of myint is"+myInt);
+	System.out.println("the value of myDouble is"+myDouble);
+	
+	double myDouble1=9.78d;
+	int myInt1=(int) myDouble1;
+	System.out.println("the value of myint1 is"+myInt1);
+	System.out.println("the value of myDouble is"+myDouble1);
+
+	}
+
+}
